912e2d59a39e592914f22ff05f8b0adaee267f70
dad7 <donald.dole@nist.gov>
2017-07-24 13:27:41 -0400

    Created wheel package. Updated setup information.

--- setup.py
+++ setup.py
@@ -2,10 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='pv_timelapse',
-    version='1.0a',
+    version='1.0.0',
     packages=find_packages(),
     author='Donald Dole',
     author_email='donald.dole@nist.gov',
+    license='CC0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Multimedia :: Video',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
     description='Generates time-lapse videos from sky camera images.',
-    install_requires=['numpy', 'pandas', 'scikit-image', 'sk-video', 'matplotlib', 'mysqlclient']
+    python_requires='>=3.5',
+    install_requires=['numpy', 'pandas', 'scikit-image', 'sk-video',
+                      'matplotlib', 'mysqlclient', 'pvlib'],
+    entry_points={
+        'console_scripts': ['pv_timelapse=pv_timelapse:main']
+    }
 )
